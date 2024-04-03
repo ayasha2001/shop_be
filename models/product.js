d4ca8beb5fddb3104548de0900af8be46e1bef7d
ayasha2001 <c.ayasha.pandey@generalaeronautics.com>
2024-04-03 19:58:38 +0530

    added code to model

--- models/product.js
+++ models/product.js
@@ -1,14 +1,41 @@
-const products=[]
-module.exports = class Product{
-    constructor(t){
-        this.title=t
-    }
+const path = require("path");
+const fs = require("fs");
 
-    save(){
-        products.push(this)
-    }
+const products = [];
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
 
-    static fetchAll(){
-        return products
-    }
-}
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "product.json"
+    );
+    fs.readFile(p, (err, fileData) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileData);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "product.json"
+    );
+    fs.readFile(p, (err, fileData) => {
+      if (err) {
+        return cb([]);
+      }
+      return cb(JSON.parse(fileData));
+    });
+  }
+};
